fc30208593819cdd1c4db2e83cc835af19589998
demonyka <cweranit@gmail.com>
2024-04-18 18:40:55 +0530

    removing

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <Head title="Главная" />
+    <h2>Hello</h2>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+import { Head, Link } from "@inertiajs/vue3";
+
+export default {
+    name: "Index",
+    components: {
+        Head,
+        Link
+    }
+}
+</script>
