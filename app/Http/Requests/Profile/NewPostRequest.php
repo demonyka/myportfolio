a7db577cfe1fd97d611ad25e35eae63278a79473
demonyka <cweranit@gmail.com>
2024-05-07 22:34:34 +0700

    fix post loading

--- app/Http/Requests/Profile/NewPostRequest.php
+++ app/Http/Requests/Profile/NewPostRequest.php
@@ -36,7 +36,7 @@ class NewPostRequest extends FormRequest
     {
 
         return [
-            'files' => 'nullable|array|max:4',
+            'files' => 'nullable|array|max:10',
             'files.*' => 'nullable|file|max:10240',
             'post' => 'required|string|min:10|max:16777215',
             'section_id' => 'required|integer'
