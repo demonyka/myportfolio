c66d05a2abdd1d13497ba2e10a061bfad6429a6a
demonyka <cweranit@gmail.com>
2024-05-08 23:27:24 +0700

    caching search

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -68,14 +68,18 @@ class ProfileService
     }
     public function find($value = '', int $limit = 5)
     {
-        return User::where('external_data->fullname', 'like', "%{$value}%")
-            ->whereNotNull('email_verified_at')
-            ->withCount(['posts as likes' => function ($query) {
-                $query->select(DB::raw('count(*)'))
-                    ->join('post_likes', 'user_posts.id', '=', 'post_likes.post_id');
-            }])
-            ->orderByDesc('likes')
-            ->limit($limit)
-            ->get();
+        $cacheKey = 'user.find.' . $value . $limit;
+        return Cache::remember($cacheKey, 60, function () use ($value, $limit) {
+            return User::where('external_data->fullname', 'like', "%{$value}%")
+                ->whereNotNull('email_verified_at')
+                ->withCount(['posts as likes' => function ($query) {
+                    $query->select(DB::raw('count(*)'))
+                        ->join('post_likes', 'user_posts.id', '=', 'post_likes.post_id');
+                }])
+                ->orderByDesc('likes')
+                ->limit($limit)
+                ->get();
+        });
+
     }
 }
